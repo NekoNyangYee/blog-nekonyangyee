6b1e15edfc3d1e44896a9e58bb32d6b2d0070fcc
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2023-08-31 22:51:08 +0900

    검색 노출 시도 3번째

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,32 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ThemeProvider from '../theme/theme';
 import GotoTop from '@/Components/GotoTop';
 import Footer from '@/Components/Footer';
-import Header from '@/Components/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'NekoNyangYee\'s Blog',
-  description: '나만의 작은 일기',
-  icons: {
-    icon: '/apple-touch-icon.png'
-  }
+export const metadata = {
+  title: "NekoNyangYee Blog",
+  description: "새로운 것을 즐기고, 변화를 만들고",
+  openGraph: {
+    type: "website",
+    url: "https://blog-nekonyangyee.vercel.app",
+    title: "NekoNyangYee Blog",
+    description: "새로운 것을 즐기고, 변화를 만들고",
+    siteName: "NekoNyangYee Blog",
+    images: [{
+      url: "/opengraph.png",
+    }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://blog-nekonyangyee.vercel.app",
+    title: "NekoNyangYee Blog",
+    description: "새로운 것을 즐기고, 변화를 만들고",
+    images: [{
+      url: "/opengraph.png",
+    }],
+  },
 }
 
 export default function RootLayout({
