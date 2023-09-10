961ebbc55a1394fd2cfc787426f7774f5ce9eb33
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2023-09-10 11:03:50 +0900

    sitemap 추가 및 manifest 수정

---
+++ sitemap.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const path = require("path");
+
+const getFileNames = () => {
+    const postsDirectory = path.join(process.cwd(), "posts");
+    return fs.readdirSync(postsDirectory);
+};
+
+const extractFrontmatter = (filecontent) => {
+    const frontmatter = {};
+    const lines = filecontent.split("\n");
+    for (const line of lines) {
+        const match = line.match(/^(\w+):\s?(.+)/);
+        if (match) {
+            const key = match[1].toLowerCase();
+            if (!frontmatter.hasOwnProperty(key)) {
+                frontmatter[key] = match[2];
+            }
+        }
+    }
+    return frontmatter;
+};
+
+const main = () => {
+    const filenames = getFileNames();
+
+    const urls = filenames.map((filename) => {
+        const filePath = path.join(process.cwd(), "posts", filename);
+        const fileContent = fs.readFileSync(filePath, "utf8");
+        const frontmatter = extractFrontmatter(fileContent);
+
+        const url = `https://blogurl.com/${filename.replace(".md", "")}`;
+        const lastmod = frontmatter.date || "";
+        return { url, lastmod };
+    });
+
+    const sitemap = `
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+          <loc>https://blogurl.com/</loc>
+        </url>
+        ${urls
+            .map(({ url, lastmod }) => {
+                return `
+              <url>
+                <loc>${url}</loc>
+                <lastmod>${lastmod}</lastmod>
+              </url>
+            `;
+            })
+            .join("")}
+      </urlset>
+    `;
+
+    fs.writeFileSync("public/sitemap.xml", sitemap);
+};
+
+main();
+
+
