bb0dbb1c224ab891462d7e1bfc7c4741c5b479de
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2023-12-16 10:42:08 +0900

    M3 스타일 적용 2

--- theme/colors.tsx
+++ theme/colors.tsx
@@ -8,33 +8,33 @@ const common = {
     lightbody: (opacity?: number) =>
         opacity ? `rgba(254, 247, 255, ${opacity / 100})` : "grey",
     lightBtnBackground: (opacity?: number) =>
-        opacity ? `rgba(103, 80, 164, ${opacity / 100})` : null,
+        opacity ? `rgba(103, 80, 164, ${opacity / 100})` : "lightBtnBackground",
     darkBtnBackground: (opacity?: number) =>
-        opacity ? `rgba(208, 188, 255, ${opacity / 100})` : null,
+        opacity ? `rgba(208, 188, 255, ${opacity / 100})` : "darkBtnBackground",
     lightbtntext: (opacity?: number) =>
-        opacity ? `rgba(255, 255, 255, ${opacity / 100})` : null,
+        opacity ? `rgba(255, 255, 255, ${opacity / 100})` : "lightbtntext",
     darkBtnText: (opacity?: number) =>
-        opacity ? `rgba(56, 30, 114, ${opacity / 100})` : null,
+        opacity ? `rgba(56, 30, 114, ${opacity / 100})` : "darkBtnText",
     lightGotoTopBtn: (opacity?: number) =>
-        opacity ? `rgba(234, 221, 255, ${opacity / 100})` : null,
+        opacity ? `rgba(234, 221, 255, ${opacity / 100})` : "lightGotoTopBtn",
     darkGotoTopBtn: (opacity?: number) =>
-        opacity ? `rgba(79, 55, 139, ${opacity / 100})` : null,
+        opacity ? `rgba(79, 55, 139, ${opacity / 100})` : "darkGotoTopBtn",
     lightGotoTopBtnText: (opacity?: number) =>
-        opacity ? `rgba(33, 0, 93, ${opacity / 100})` : null,
+        opacity ? `rgba(33, 0, 93, ${opacity / 100})` : "lightGotoTopBtnText",
     darkGotoTopBtnText: (opacity?: number) =>
-        opacity ? `rgba(234, 221, 255, ${opacity / 100})` : null,
+        opacity ? `rgba(234, 221, 255, ${opacity / 100})` : "darkGotoTopBtnText",
     lightHeaderBackground: (opacity?: number) =>
-        opacity ? `rgba(243, 237, 247, ${opacity / 100})` : null,
+        opacity ? `rgba(243, 237, 247, ${opacity / 100})` : "lightHeaderBackground",
     darkHeaderBackground: (opacity?: number) =>
-        opacity ? `rgba(33, 31, 38, ${opacity / 100})` : null,
+        opacity ? `rgba(33, 31, 38, ${opacity / 100})` : "darkHeaderBackground",
     lightInputBackground: (opacity?: number) =>
-        opacity ? `rgba(230, 224, 233, ${opacity / 100})` : null,
+        opacity ? `rgba(230, 224, 233, ${opacity / 100})` : "lightInputBackground",
     darkInputBackground: (opacity?: number) =>
-        opacity ? `rgba(54, 52, 59, ${opacity / 100})` : null,
+        opacity ? `rgba(54, 52, 59, ${opacity / 100})` : "darkInputBackground",
     lightInputBorder: (opacity?: number) =>
-        opacity ? `rgba(73, 69, 79, ${opacity / 100})` : null,
+        opacity ? `rgba(73, 69, 79, ${opacity / 100})` : "lightInputBorder",
     darkInputBorder: (opacity?: number) =>
-        opacity ? `rgba(202, 196, 208, ${opacity / 100})` : null,
+        opacity ? `rgba(202, 196, 208, ${opacity / 100})` : "darkInputBorder",
 };
 
 const light = {
